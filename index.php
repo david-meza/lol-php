199111b4dab9ff5aceef26fddd41b6d69a7b31f7
David <davidmeza@outlook.com>
2015-10-03 22:44:25 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <title>Lol PHP</title>
+</head>
+<body>
+
+  <p>
+    <?php
+    // Write your function below!
+    function displayName() {
+        echo "David";
+    }
+
+    displayName();
+    ?>
+  </p>
+
+</body>
+</html>
